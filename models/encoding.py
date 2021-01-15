8cc9347dbfb57c3f0bee732f4540ba9fbf334472
eddie <aigagror@gmail.com>
2021-01-15 13:35:52 -0600

    added groupnorm to encoding net

--- models/encoding.py
+++ models/encoding.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+import tensorflow_addons as tfa
 
 
 def encode(args, img, out_dim):
@@ -15,6 +16,7 @@ def encode(args, img, out_dim):
         prefix = 'first-encode-block'
         output = keras.Sequential([
             layers.Conv2D(16, 1, name=f'{prefix}-conv'),
+            tfa.layers.GroupNormalization(groups=1),
             layers.LeakyReLU(0.2)
         ], prefix)(img)
 
@@ -27,8 +29,10 @@ def encode(args, img, out_dim):
             in_h, out_h = hdims[i], hdims[i + 1]
             output = keras.Sequential([
                 layers.Conv2D(in_h, 3, padding='same', name=f'{prefix}-conv1'),
+                tfa.layers.GroupNormalization(groups=1),
                 layers.LeakyReLU(0.2),
                 layers.Conv2D(out_h, 3, padding='same', name=f'{prefix}-conv2'),
+                tfa.layers.GroupNormalization(groups=1),
                 layers.LeakyReLU(0.2),
                 layers.AveragePooling2D(),
             ], prefix)(output)
@@ -39,8 +43,10 @@ def encode(args, img, out_dim):
         prefix = f'last-encode-block{i}'
         output = keras.Sequential([
             layers.Conv2D(out_h, 3, padding='same', name=f'{prefix}-conv1'),
+            tfa.layers.GroupNormalization(groups=1),
             layers.LeakyReLU(0.2),
             layers.Conv2D(out_h, 4, padding='valid', name=f'{prefix}-conv2'),
+            tfa.layers.GroupNormalization(groups=1),
             layers.LeakyReLU(0.2),
             layers.Flatten(),
             layers.Dense(out_dim, name=f'{prefix}-dense')
