c72f211cc5b48dbd141a3084aaa9c0b920466dd2
eddie <aigagror@gmail.com>
2021-01-17 17:59:21 -0600

    removed BN from encoding

--- models/encoding.py
+++ models/encoding.py
@@ -16,7 +16,6 @@ def encode(args, img, out_dim):
         prefix = 'first-encode-block'
         output = keras.Sequential([
             tfa.layers.SpectralNormalization(layers.Conv2D(16, 1), name=f'{prefix}-conv'),
-            layers.BatchNormalization(name=f'{prefix}-norm'),
             layers.LeakyReLU(0.1)
         ], prefix)(img)
 
@@ -30,11 +29,9 @@ def encode(args, img, out_dim):
             output = keras.Sequential([
                 tfa.layers.SpectralNormalization(layers.Conv2D(in_h, 3, padding='same'),
                                                  name=f'{prefix}-conv1'),
-                layers.BatchNormalization(name=f'{prefix}-norm1'),
                 layers.LeakyReLU(0.1),
                 tfa.layers.SpectralNormalization(layers.Conv2D(out_h, 3, padding='same'),
                                                  name=f'{prefix}-conv2'),
-                layers.BatchNormalization(name=f'{prefix}-norm2'),
                 layers.LeakyReLU(0.1),
                 layers.AveragePooling2D(),
             ], prefix)(output)
@@ -46,11 +43,9 @@ def encode(args, img, out_dim):
         output = keras.Sequential([
             tfa.layers.SpectralNormalization(layers.Conv2D(out_h, 3, padding='same'),
                                              name=f'{prefix}-conv1'),
-            layers.BatchNormalization(name=f'{prefix}-norm1'),
             layers.LeakyReLU(0.1),
             tfa.layers.SpectralNormalization(layers.Conv2D(out_h, 4, padding='valid'),
                                              name=f'{prefix}-conv2'),
-            layers.BatchNormalization(name=f'{prefix}-norm2'),
             layers.LeakyReLU(0.1),
             layers.Flatten(),
             tfa.layers.SpectralNormalization(layers.Dense(out_dim), name=f'{prefix}-dense')
