6959bc1822ea93175c40a6198496e3e9aa03e620
eddie <aigagror@gmail.com>
2021-01-30 12:58:38 -0600

    refactor

--- models/encoding.py
+++ models/encoding.py
@@ -6,13 +6,13 @@ from tensorflow.keras import layers
 
 def encode(args, img, out_dim):
     if args.encoder == 'affine':
-        output = layers.Flatten()(img)
-        output = layers.Dense(out_dim, name='affine-encoder')(output)
+        out = layers.Flatten()(img)
+        out = layers.Dense(out_dim, name='affine-encoder')(out)
 
     elif args.encoder == 'conv':
         # First block
-        output = tfa.layers.SpectralNormalization(layers.Conv2D(16, 1), name=f'first-encode-block-conv')(img)
-        output = layers.LeakyReLU(0.1)(output)
+        out = tfa.layers.SpectralNormalization(layers.Conv2D(16, 1), name=f'first-encode-block-conv')(img)
+        out = layers.LeakyReLU(0.1)(out)
 
         # Hidden blocks
         hdims = [min(16, args.hdim), min(32, args.hdim), min(64, args.hdim), min(128, args.hdim), min(256, args.hdim),
@@ -22,34 +22,34 @@ def encode(args, img, out_dim):
             prefix = f'hidden-encode-block{i}'
             in_h, out_h = hdims[i], hdims[i + 1]
 
-            output = tfa.layers.SpectralNormalization(layers.Conv2D(in_h, 3, padding='same'),
-                                                      name=f'{prefix}-conv1')(output)
-            output = layers.LeakyReLU(0.1)(output)
+            out = tfa.layers.SpectralNormalization(layers.Conv2D(in_h, 3, padding='same'),
+                                                      name=f'{prefix}-conv1')(out)
+            out = layers.LeakyReLU(0.1)(out)
 
-            output = tfa.layers.SpectralNormalization(layers.Conv2D(out_h, 3, padding='same'),
-                                                      name=f'{prefix}-conv2')(output)
-            output = layers.LeakyReLU(0.1)(output)
+            out = tfa.layers.SpectralNormalization(layers.Conv2D(out_h, 3, padding='same'),
+                                                      name=f'{prefix}-conv2')(out)
+            out = layers.LeakyReLU(0.1)(out)
 
-            output = layers.AveragePooling2D()(output)
+            out = layers.AveragePooling2D()(out)
 
-            if output.shape[1] == 4:
+            if out.shape[1] == 4:
                 break
 
         # Last block
         prefix = f'last-encode-block{i}'
-        output = tfa.layers.SpectralNormalization(layers.Conv2D(out_h, 3, padding='same'),
-                                                  name=f'{prefix}-conv1')(output)
-        output = layers.LeakyReLU(0.1)(output)
+        out = tfa.layers.SpectralNormalization(layers.Conv2D(out_h, 3, padding='same'),
+                                                  name=f'{prefix}-conv1')(out)
+        out = layers.LeakyReLU(0.1)(out)
 
-        output = tfa.layers.SpectralNormalization(layers.Conv2D(out_h, 4, padding='valid'),
-                                                  name=f'{prefix}-conv2')(output)
-        output = layers.LeakyReLU(0.1)(output)
+        out = tfa.layers.SpectralNormalization(layers.Conv2D(out_h, 4, padding='valid'),
+                                                  name=f'{prefix}-conv2')(out)
+        out = layers.LeakyReLU(0.1)(out)
 
-        output = layers.Flatten()(output)(output)
-        output = tfa.layers.SpectralNormalization(layers.Dense(out_dim), name=f'{prefix}-dense')(output)
+        out = layers.Flatten()(out)(out)
+        out = tfa.layers.SpectralNormalization(layers.Dense(out_dim), name=f'{prefix}-dense')(out)
     else:
         raise Exception(f'unknown encoder network {args.encoder}')
 
     # Encoder output
-    tf.debugging.assert_shapes([(output, tf.TensorShape([None, out_dim]))])
-    return output
+    tf.debugging.assert_shapes([(out, tf.TensorShape([None, out_dim]))])
+    return out
